b7afa9fade19e1862c06bd7009feafa7a8365175
AMSEDev <alex2001@gmail.com>
2023-10-08 23:41:07 -0500

    Ventas 1.2

--- src/router/index.ts
+++ src/router/index.ts
@@ -103,11 +103,11 @@ const router = createRouter({
           name: 'new-sale',
           component: () => import('../views/sales/nuevo.vue')
         },
-        {
-          path: 'ventas/edit/:id',
-          name: 'edit-sale',
-          component: () => import('../views/sales/edit.vue')
-        },
+        // {
+        //   path: 'ventas/edit/:id',
+        //   name: 'edit-sale',
+        //   component: () => import('../views/sales/edit.vue')
+        // },
 
         {
           path: 'roles',
